b743223ade7708669e614bde81403bf8fd3d07f0
Lucas Giacone <68192656+lucasGiacone@users.noreply.github.com>
2020-07-18 03:44:56 -0300

    Mudanças na pagina de assitir os videos

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let a = document.getElementsByClassName("landscape-element block-link cf titlefi
 for (let i = 0; i < a.length; i++) {
     a[i].setAttribute("class", "landscape-element-modified block-link cf titlefix js-featured-show-link")
     }
+let b = document.getElementsByClassName("landscape-element block-link cf titlefix")
+while (b.length){
+    for (let j = 0; j < b.length; j++) {
+        b[j].setAttribute("class", "landscape-element-modified block-link cf titlefix")
+    } 
+}
