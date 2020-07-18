5469417987735bba637ad7ed4d8ff54d40722e0c
Lucas Giacone <68192656+lucasGiacone@users.noreply.github.com>
2020-07-18 02:15:19 -0300

    main page pronta

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 document.getElementById("games_button").setAttribute("id", "games_btn");
+let a = document.getElementsByClassName("landscape-element block-link cf titlefix js-featured-show-link")
+for (let i = 0; i < a.length; i++) {
+    a[i].setAttribute("class", "landscape-element-modified block-link cf titlefix js-featured-show-link")
+    }
